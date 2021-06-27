4073d5e6a138fdb721317c5ef1d9b679e3c0d1ae
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-27 14:27:51 -0400

    commit tree a07e664a6bdb4257a61038970eb71ef3c580f95d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+require 'pry'
+
+def line(people)
+    if people.empty?
+        puts "The line is currently empty."
+    else 
+        string_builder = "The line is currently:" 
+        people.each_with_index do |person, index|
+            string_builder += " #{index + 1}. #{person}"
+        end
+        puts string_builder
+    end
+end
+
+
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+
+def now_serving(line)
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{line[0]}."
+        line.shift
+    end
+end
+
+
+
